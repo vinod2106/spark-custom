3a0c2e66f8fc18307c55bfa5f466eecec0b7210d
vinod2106 <vinod254581@gmail.com>
2017-11-02 12:16:21 +0100

    New filter example to show diff between java 7 and 8

---
+++ src/main/java/com/shavinod/spark/funcs/SearchExample.java
@@ -0,0 +1,73 @@
+package com.shavinod.spark.funcs;
+
+import java.net.URL;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SearchExample {
+
+	// set logger class
+	private static final Logger logger = LoggerFactory.getLogger(SearchExample.class);
+	private static String inputFilePath;
+
+	/**
+	 * This methods accepts a file and prints the no of lines containing string
+	 * error.
+	 * 
+	 * @param sc
+	 * @param filePath
+	 */
+	public static void oldJavaFilter(JavaSparkContext sc, String filePath) {
+		// read and filter
+		JavaRDD<String> lines = sc.textFile(inputFilePath).filter(new Function<String, Boolean>() {
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Boolean call(String s) throws Exception {
+				// TODO Auto-generated method stub
+
+				return s.contains("error");
+			}
+		});
+
+		long numErrors = lines.count();
+		logger.info("numErrors ==========> {}", numErrors);
+
+	}
+
+	private static void newJavaFilter(JavaSparkContext sc, String filePath) {
+		// read and filter
+		JavaRDD<String> lines = sc.textFile(inputFilePath).filter(s->s.contains("error"));
+		long numErrors = lines.count();
+		logger.info("numErrors ==========> {}", numErrors);
+	}
+
+	public static void main(String[] args) {
+
+		SparkConf sconf = new SparkConf().setAppName("TestFunctions").setMaster("local[*]");
+		JavaSparkContext sc = new JavaSparkContext(sconf);
+		sc.setLogLevel("INFO");
+		// input file
+		String inputFile = args[0];
+		logger.info("inputFile =======> " + inputFile);
+
+		// Read the input file name from classLoader. Just give the name of the file not
+		// complete path
+		// InputStream resourceContent = classLoader.getResourceAsStream(inputFile);
+		URL inputFileURL = ClassLoader.getSystemResource(inputFile);
+		logger.info("inputFileURL" + inputFileURL.toString());
+		inputFilePath = inputFileURL.getPath();
+
+		newJavaFilter(sc, inputFilePath);
+
+		sc.close();
+	}
+}
